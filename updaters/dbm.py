8cab40e20881e9e10a0b7c7a140c461c570c4829
Maiku Mori <MiksKalnins@MaikuMori.com>
2011-10-10 16:38:12 +0300

    *Fixed spelling mistake in dbm.py
    *Changed how DBM alpha versions are detected to reflect the changes they made on the site. (Should now always update to the most recent alpha)

--- updaters/dbm.py
+++ updaters/dbm.py
@@ -1,5 +1,6 @@
-"""Custom updater for Deadly Boss Mods alphaa versions."""
+"""Custom updater for Deadly Boss Mods alpha versions."""
 import re, urllib2
+import httplib
 
 from updaters import Updater
 from helper_functions import *
@@ -24,19 +25,31 @@ class DeadlyBossModsUpdater(Updater):
         return False
 	
     def _canUpdate(self):
-        #http://www.deadlybossmods.com/download.php?id=6
-        #<a href="http://www.deadlybossmods.com/download.php?id=6">4.83-r6008-alpha</a>
-        source = urllib2.urlopen('http://www.ninjapull.de/index.php?option=com_content&view=article&id=100&Itemid=121&lang=en')
-        myregex = re.compile('>([0-9]+?\.[0-9]+?\.[0-9]+?-alpha)<')
-        m = myregex.search(source.read())
-        if m:
+        # source = urllib2.urlopen('http://www.ninjapull.de/index.php?option=com_content&view=article&id=100&Itemid=121&lang=en')
+        # myregex = re.compile('>([0-9]+?\.[0-9]+?\.[0-9]+?-alpha)<')
+        # m = myregex.search(source.read())
+        # if m:
+            # self.file = 'http://www.ninjapull.de/dbm/download.php?id=6'
+            # self.last_version = m.group(1)
+            # if self.last_version != self.current_version:
+                # return True
+            # else:
+                # return False
+        # else:
+            # if self.logging:
+                # self.logging.error("Regex didn't match, probably download URL has changed. (Deadly Boss Mods)")
+            # return False
+            
+            
+        conn = httplib.HTTPConnection("www.ninjapull.de")
+        conn.request("HEAD", "/dbm/download.php?id=6")
+        resp = conn.getresponse()
+        #'attachment; filename="DBM-svn-4.10.0-r6576-alpha.zip"'
+        #                       ^                        ^
+        #                      22                       -5
+        self.last_version = resp.getheader("content-disposition")[22:-5]
+        if self.last_version != self.current_version:
             self.file = 'http://www.ninjapull.de/dbm/download.php?id=6'
-            self.last_version = m.group(1)
-            if self.last_version != self.current_version:
-                return True
-            else:
-                return False
+            return True
         else:
-            if self.logging:
-                self.logging.error("Regex didn't match, probably download URL has changed. (Deadly Boss Mods)")
             return False
